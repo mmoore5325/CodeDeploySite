c7bb398cf6c73f9b5536b666ebdf5b89da568692
mmoore5325 <mmoore5325@gmail.com>
2017-09-13 23:00:08 -0400

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-(cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd /var/www && nohup sudo bundle install && sudo ruby app.rb -o 0.0.0.0 &)
-expect "nohup: ignoring input and appending output to '/home/ubuntu/nohup.out'" { send "\r" }
+(cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd /var/www && sudo bundle install && sudo ruby app.rb -o 0.0.0.0 &)
 echo "working?"
 exit 0
 
