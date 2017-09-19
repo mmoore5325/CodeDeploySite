aec8082979df1722990b61ca75c75ad148c3cd2e
mmoore5325 <mmoore5325@gmail.com>
2017-09-19 00:07:48 -0400

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-(cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd .. && cd /var/www && sudo bundle install && sudo ruby app.rb -o 0.0.0.0 &)
+(
+cd /var/www 
+sudo bundle install
+sudo ruby app.rb -o 0.0.0.0 &
+)
 echo "working?"
 exit 0
 
